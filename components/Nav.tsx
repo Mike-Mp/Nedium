d4ec48ebfda3b3ab74d02a66eb5de82e01a1d544
Mike-Mp <mikemp@protonmail.com>
2022-01-21 22:16:07 +0200

    nav htmling

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
+
 export default function Nav() {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-teal p-6">
-        <div className="flex items-center flex-no-shrink text-white mr-6">
-          <svg className="h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-          <span className="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+        <div className="flex items-center flex-no-shrink text-black mr-6">
+            <Link href="/">
+                <a className="font-semibold text-xl tracking-tight">Nedium</a>
+            </Link>       
         </div>
         <div className="block lg:hidden">
           <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
@@ -12,18 +15,24 @@ export default function Nav() {
         </div>
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow">
-            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-              Docs
-            </a>
-            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-              Examples
-            </a>
-            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white">
-              Blog
-            </a>
+            <Link href="/about" >
+                <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter mr-4">
+                    About
+                </a>
+            </Link>
+            <Link href="/login" >
+                <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter mr-4">
+                    Sign in
+                </a>
+            </Link>
+            
           </div>
           <div>
-            <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Download</a>
+            <Link href="/register">
+                <a className="inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-white hover:bg-black mt-4 lg:mt-0">
+                    Sign Up   
+                </a>
+            </Link>
           </div>
         </div>
       </nav>
